352c9e76b193aeb461b9cb83ce090ede9aad7f45
dmscola <dmscola@umich.edu>
2018-01-29 14:18:34 -0500

    Fixed functional errors and updated syntax

--- magic_eight.py
+++ magic_eight.py
@@ -1,11 +1,20 @@
-<<<<<<< HEAD
+
 import random
+
+all_answers=["It is certain", "It is decidedly so", "Without a doubt",
+              "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
+			  "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+			  "Ask again later", "Better not tell you now", "Cannot predict now",
+			  "Concentrate and ask again", "Don't count on it", "My reply is no",
+			  "My sources say no", "Outlook not so good", "Very doubtful"
+			  ]
+
 
 def question():
 	print("What is your question?")
 	question = input()
 	return question
-=======
+
 user_question = ""
 while(user_question!= "quit"):
 
@@ -18,17 +27,10 @@ while(user_question!= "quit"):
 	user_question = question()
 
 	if user_question.endswith("?"):
-		pass
+		answer=print(random.choice(all_answers))
+
 	else:
 		print("I’m sorry, I can only answer questions.")
->>>>>>> check_question
 
-all_answers=["It is certain", "It is decidedly so", "Without a doubt",
-              "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
-			  "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
-			  "Ask again later", "Better not tell you now", "Cannot predict now",
-			  "Concentrate and ask again", "Don't count on it", "My reply is no",
-			  "My sources say no", "Outlook not so good", "Very doubtful"
-			  ]
 
-answer=print(random.choice(all_answers))
+
