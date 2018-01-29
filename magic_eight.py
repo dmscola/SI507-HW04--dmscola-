14df962b4a0f348d9fabb2bccb7ab02b1adae57e
dmscola <dmscola@umich.edu>
2018-01-29 14:00:43 -0500

    Added loop and found whether or not input ends with a question mark

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,16 @@
-def question():
-	print("What is your question?")
-	question = input()
-	return question
+user_question = ""
+while(user_question!= "quit"):
+
+
+	def question():
+		print("What is your question?")
+		question = input()
+		return question
+
+	user_question = question()
+
+	if user_question.endswith("?"):
+		pass
+	else:
+		print("I’m sorry, I can only answer questions.")
 
